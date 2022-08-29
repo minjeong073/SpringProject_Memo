0121e158d00bc0fba63881b05ccaf4b63dec9ec6
DESKTOP-4OK39MV\user <kim.mj80730@gmail.com>
2022-08-29 18:27:34 +0900

    [0829] memo project - 1st commit

---
+++ src/main/java/com/ming/project/memo/SpringProjectMemoApplication.java
@@ -0,0 +1,13 @@
+package com.ming.project.memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectMemoApplication.class, args);
+	}
+
+}
