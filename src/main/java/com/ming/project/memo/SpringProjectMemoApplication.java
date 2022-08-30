00a67c6f6a481b8d34c84a49c5b630684ec6508e
DESKTOP-4OK39MV\user <>
2022-08-30 16:11:28 +0900

    [0830]  spring project - yml 을 통한 jsp database 설정 - jsp 관련 build.gradle 수정 - minjeong073/SpringProject_Memo#1 이슈 정리

--- src/main/java/com/ming/project/memo/SpringProjectMemoApplication.java
+++ src/main/java/com/ming/project/memo/SpringProjectMemoApplication.java
@@ -3,6 +3,7 @@ package com.ming.project.memo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SpringProjectMemoApplication {
 
---
+++ src/main/java/com/ming/project/memo/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.ming.project.memo.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String helloWorld() {
+		return "hello world!";
+	}
+}
